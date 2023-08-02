dcd945ceb416bb8824432708effd68f2f7fc7c7e
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-02 09:45:19 +0000

    implement post views+

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Location;
 use App\Models\Post;
+use App\Models\PostView;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -45,7 +46,7 @@ class PostController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Post $post)
+    public function show(Post $post, Request $request)
     {
         if(!$post->active || $post->published_at > Carbon::now()) {
             throw new NotFoundHttpException;
@@ -66,6 +67,16 @@ class PostController extends Controller
             ->orderBy('published_at', 'asc')
             ->limit(1)
             ->first();
+
+        /** @var \App\Models\User $user */
+        $user = $request->user();
+
+        PostView::create([
+            'ip_address' => $request->ip(),
+            'user_agent' => $request->userAgent(),
+            'user_id' => $user?->id,
+            'post_id' => $post->id
+        ]);
 
         return view('post.view', compact('post', 'prev', 'next'));
     }
